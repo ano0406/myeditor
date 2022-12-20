6b4d38b7263d7acaaf152023041f00307e12aae5
菊池雅英 <kikuchi.masahide.86n@st.kyoto-u.ac.jp>
2022-12-21 08:22:34 +0900

    バックエンド側での名前重複チェックを一時取りやめ

--- app/Http/Controllers/FilesRestController.php
+++ app/Http/Controllers/FilesRestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+//use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 use App\Models\File;
 
 use Illuminate\Support\Facades\Log;
@@ -26,17 +27,8 @@ class FilesRestController extends Controller
     }
 
     /**
-     *
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-    }
-
-    /**
      * ファイルの作成 ファイル本文(name)とテキスト(text)をリクエストに含める
-     * レスポンスとして、割り当てられた新規ファイルidと成功か否かをjson({success:boolean,id:number})として返す
+     * 作成したファイルに割り当てられたidを返す(response:{id:number})
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
@@ -47,52 +39,38 @@ class FilesRestController extends Controller
         $result = $request->all();
         $name = $request->input('name');
         $text = $request->input('text');
-        $success = true;
-        //同名のファイルがないかチェック
-        if(File::where('name',$name)->exists()){
-            $success = false;
-        }
-        if($success){
-            $file = new File;
-            $file->user_id = $user->id;
-            $file->name = $name;
+        $file = new File;
+        $file->user_id = $user->id;
+        $file->name = $name;
+        if($file->text != null){
             $file->text = $text;
-            $file->save();
-            return response()->json([
-                'success' => true,
-                'id' => $file->id,
-            ]);
         }else{
-            return response()->json([
-                'success' => false,
-            ]);
+            $file->text = '';
         }
+        $file->save();
+        return response()->json([
+            'id' => $file->id,
+        ]);
     }
 
     /**
-     * ファイルidに対し、
-     * ・取得成功か否か(success:boolean)
-     * ・成功ならば指定idのファイル名(name:string)とファイル本文(text:string)
-     * を、jsonで返す
+     * ファイルidに対し、ユーザーが保持していればファイル名(name:string)とファイル本文(text:string)を、jsonで返す
+     * そうでなければ400を返す
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    //TODO:取得できなかったら別のエラーコードで返した方が、処理が楽
     public function show($id)
     {
         $user = Auth::user();
         $file = File::find($id);
         if($file != null and $file->user_id == $user->id){
             return response()->json([
-                'success' => true,
                 'name' => $file->name,
                 'text' => $file->text,
             ]);
         }else{
-            return response()->json([
-                'success' => false,
-            ]);
+            abort(400);
         }
     }
 
@@ -100,7 +78,7 @@ class FilesRestController extends Controller
      * 指定idを持つファイルのファイル名、中身を更新する
      * $requestには新規ファイル名(name)またはファイルの中身(text)を含める
      * $requestにない項目は更新しない
-     * レスポンスとして、更新成功か否か(success:boolean)を持つjsonを返す
+     * 更新できない(ユーザーが保持するファイルではないなど)ならば400を返す
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
@@ -111,34 +89,23 @@ class FilesRestController extends Controller
         $userid = Auth::user()->id;
         $file = File::find($id);
         if($file == null or $file->user_id != $userid){
-            return response()->json([
-                'success' => false,
-            ]);
+            abort(400);
         }
         $name = $request->name;
         $text = $request->text;
         if($name != null){
-            //同じユーザーの同名ファイルがあれば更新しない
-            $samename_file = File::select('id')->where('user_id',$userid)->where('name',$name)->first();
-            if($samename_file != null and $samename_file->id != $id){
-                return response()->json([
-                    'success' => false,
-                ]);
-            }
             $file->name = $name;
         }
         if($text != null){
             $file->text = $text;
         }
         $file->save();
-        return response()->json([
-            'success' => true,
-        ]);
+        return response()->json([]);
     }
 
     /**
      * 指定idを持つファイルを削除する
-     * レスポンスとして、削除成功か否か(success:boolean)を持つjsonを返す
+     * 削除失敗(ユーザーがファイルを所有していないなど)ならば400を返す
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
@@ -148,14 +115,10 @@ class FilesRestController extends Controller
         $userid = Auth::user()->id;
         $file = File::find($id);
         if($file == null or $file->user_id != $userid){
-            return response()->json([
-                'success' => false,
-            ]);
+            abort(400);
         }else{
             $file->delete();
-            return response()->json([
-                'success' => true,
-            ]);
+            return response()->json([]);
         }
     }
 }
